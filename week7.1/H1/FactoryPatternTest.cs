2b49979afe8f22c45c72e79b956e3c9a763588c1
maruf128 <maruf@live.nl>
2023-05-29 11:35:33 +0200

    projecten na kijken

--- week7.1/H1/FactoryPatternTest.cs
+++ week7.1/H1/FactoryPatternTest.cs
@@ -1,64 +1,64 @@
-namespace FactoryPattern.Tests
-{
-    [TestClass]
-    public class FactoryPatternTest
-    {
-        [TestMethod]
-        public void CreateLightTheme_Document()
-        {
-            // Arrange
-            DocumentFactory factory = new DocumentFactory();
+// namespace FactoryPattern.Tests
+// {
+//     [TestClass]
+//     public class FactoryPatternTest
+//     {
+//         [TestMethod]
+//         public void CreateLightTheme_Document()
+//         {
+//             // Arrange
+//             DocumentFactory factory = new DocumentFactory();
 
-            // Act
-            Document document = factory.CreateLightTheme();
+//             // Act
+//             Document document = factory.CreateLightTheme();
 
-            // Assert
-            Assert.AreEqual("Black", document.FontColor);
-            Assert.AreEqual("White", document.BackGroundColor);
-        }
+//             // Assert
+//             Assert.AreEqual("Black", document.FontColor);
+//             Assert.AreEqual("White", document.BackGroundColor);
+//         }
 
-        [TestMethod]
-        public void CreateDarkTheme_Document()
-        {
-            // Arrange
-            DocumentFactory factory = new DocumentFactory();
+//         [TestMethod]
+//         public void CreateDarkTheme_Document()
+//         {
+//             // Arrange
+//             DocumentFactory factory = new DocumentFactory();
 
-            // Act
-            Document document = factory.CreateDarkTheme();
+//             // Act
+//             Document document = factory.CreateDarkTheme();
 
-            // Assert
-            Assert.AreEqual("White", document.FontColor);
-            Assert.AreEqual("Black", document.BackGroundColor);
-        }
+//             // Assert
+//             Assert.AreEqual("White", document.FontColor);
+//             Assert.AreEqual("Black", document.BackGroundColor);
+//         }
 
-        [TestMethod]
-        public void CreateLightTheme_IDE()
-        {
-            // Arrange
-            IDEFactory factory = new IDEFactory();
+//         [TestMethod]
+//         public void CreateLightTheme_IDE()
+//         {
+//             // Arrange
+//             IDEFactory factory = new IDEFactory();
 
-            // Act
-            IDE ide = factory.CreateLightTheme();
+//             // Act
+//             IDE ide = factory.CreateLightTheme();
 
-            // Assert
-            Assert.AreEqual("Black", ide.FontColor);
-            Assert.AreEqual("White", ide.BackGroundColor);
-            Assert.AreEqual("Light blue", ide.ButtonColor);
-        }
+//             // Assert
+//             Assert.AreEqual("Black", ide.FontColor);
+//             Assert.AreEqual("White", ide.BackGroundColor);
+//             Assert.AreEqual("Light blue", ide.ButtonColor);
+//         }
 
-        [TestMethod]
-        public void CreateDarkTheme_IDE()
-        {
-            // Arrange
-            IDEFactory factory = new IDEFactory();
+//         [TestMethod]
+//         public void CreateDarkTheme_IDE()
+//         {
+//             // Arrange
+//             IDEFactory factory = new IDEFactory();
 
-            // Act
-            IDE ide = factory.CreateDarkTheme();
+//             // Act
+//             IDE ide = factory.CreateDarkTheme();
 
-            // Assert
-            Assert.AreEqual("White", ide.FontColor);
-            Assert.AreEqual("Black", ide.BackGroundColor);
-            Assert.AreEqual("Light yellow", ide.ButtonColor);
-        }
-    }
-}
+//             // Assert
+//             Assert.AreEqual("White", ide.FontColor);
+//             Assert.AreEqual("Black", ide.BackGroundColor);
+//             Assert.AreEqual("Light yellow", ide.ButtonColor);
+//         }
+//     }
+// }
